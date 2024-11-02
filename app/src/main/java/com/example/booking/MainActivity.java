cb3484e643814646ba4501d0e0a8c34c5253f39b
BAEK0111 <baekjw111@gmail.com>
2024-11-02 19:02:15 +0900

    바텀 네비게이션 화면 전환 구현

--- app/src/main/java/com/example/booking/MainActivity.java
+++ app/src/main/java/com/example/booking/MainActivity.java
@@ -7,18 +7,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.booking.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initBottomNavigation();
+    }
+
+    private void initBottomNavigation() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_frm, new HomeFragment())
+                .commitNowAllowingStateLoss();
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.homeFragment) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.mapFragment) {
+                selectedFragment = new MapFragment();
+            } else if (itemId == R.id.recordFragment) {
+                selectedFragment = new RecordFragment();
+            } else if (itemId == R.id.myFragment) {
+                selectedFragment = new MyFragment();
+            }
+            if(selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_frm, selectedFragment)
+                        .commitNowAllowingStateLoss();
+                return true;
+            } else {
+                return false;
+            }
         });
     }
 }
