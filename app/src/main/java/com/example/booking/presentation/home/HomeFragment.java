d42950d07803e24ec21076f9dff33abe80d796f9
hyowon <ally0204@naver.com>
2024-11-07 23:25:09 +0900

    [FEAT/#1] HomeFragment 에서 임시 화면전환 설정

--- app/src/main/java/com/example/booking/presentation/home/HomeFragment.java
+++ app/src/main/java/com/example/booking/presentation/home/HomeFragment.java
@@ -3,19 +3,36 @@ package com.example.booking.presentation.home;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.booking.R;
+import com.example.booking.presentation.search.BookSearchFragment;
 
 public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        EditText openBookSearchEditText = view.findViewById(R.id.et_home_search);
+        openBookSearchEditText.setFocusable(false);
+        openBookSearchEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // BookSearchFragment로 전환
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_frm, new BookSearchFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 }
