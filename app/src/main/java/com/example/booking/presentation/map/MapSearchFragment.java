e88905c9cba2cc433a74c01c7f0f1733635862bc
BAEK0111 <baekjw111@gmail.com>
2024-11-08 22:38:01 +0900

    [FEAT/#34] 독서 맵 -> 독서 맵 검색하기 네비게이션 구현

---
+++ app/src/main/java/com/example/booking/presentation/map/MapSearchFragment.java
@@ -0,0 +1,20 @@
+package com.example.booking.presentation.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.booking.R;
+
+public class MapSearchFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map_search, container, false);
+
+        return view;
+    }
+}
