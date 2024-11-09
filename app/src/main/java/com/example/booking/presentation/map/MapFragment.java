35b920d6f95de5fb6290324ba33186fd84c146c2
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2024-11-09 20:11:41 +0900

    [Feat/#34] click event1 (#39)
    * [FEAT/#34] 독서 맵 
    - 독서 맵 검색하기 네비게이션 구현
    
    * [FEAT/#34] 독서맵 검색하기 
    - 독서 맵 네비게이션 구현
    
    * [FEAT/#34]
    - 기록 등록하기 (장소 클릭) -> 장소 검색
    - 장소 검색 (뒤로 가기)-> 기록 등록하기
    
    * [FEAT/#34]
    - 기록 등록하기 (독서 종료) -> 바텀 시트
    
    * [FEAT/#34]
    - 바텀 시트 스타일 변경
    - 독서 맵 검색화면 바탕화면 색상 변경
    
    * [FEAT/#34]
    - 바텀 시트 -> 기록 모아보기
    
    * [FEAT/#34]
    - 최종 독서 기록 클래스 생성
    - 파이어베이스 연동을 위한 의존성 주입
    - 바텀 시트 배경 삭제를 위해서 스타일 변경

--- app/src/main/java/com/example/booking/presentation/map/MapFragment.java
+++ app/src/main/java/com/example/booking/presentation/map/MapFragment.java
@@ -3,10 +3,14 @@ package com.example.booking.presentation.map;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.booking.R;
 
@@ -16,6 +20,16 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // NavController 가져오기
+        NavController navController = Navigation.findNavController(container);
+
+        // 버튼 설정 및 클릭 리스너 추가
+        EditText editText = view.findViewById(R.id.et_home_search);
+        editText.setOnClickListener(v -> {
+            navController.navigate(R.id.action_mapFragment_to_mapSearchFragment);
+        });
+        return view;
     }
 }
