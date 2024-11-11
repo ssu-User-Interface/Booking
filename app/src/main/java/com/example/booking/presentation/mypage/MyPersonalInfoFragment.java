82df5eb2430b115b1f2aa44088637cedf9ab9869
hyowon <ally0204@naver.com>
2024-11-11 23:07:32 +0900

    [FEAT/#45] myFragment에서 MyPersonalInfoFragment 사이의 화면 전환 구현

---
+++ app/src/main/java/com/example/booking/presentation/mypage/MyPersonalInfoFragment.java
@@ -0,0 +1,33 @@
+package com.example.booking.presentation.mypage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.booking.R;
+
+public class MyPersonalInfoFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_personal_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = NavHostFragment.findNavController(this);
+
+        ImageView backButton = view.findViewById(R.id.iv_back_arrow);
+        backButton.setOnClickListener(v ->
+                navController.navigateUp()
+        );
+    }
+}
