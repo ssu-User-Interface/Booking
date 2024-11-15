799eae5cb626eeff8c2ce1262f20a88e8f1ebdd7
BAEK0111 <baekjw111@gmail.com>
2024-11-15 11:03:11 +0900

    [FIX/#40] - AndroidManifest에 해시키값 넣기 - 의존성 추가 - 카카오맵 가져오기 완료

--- app/src/main/java/com/example/booking/presentation/map/MapFragment.java
+++ app/src/main/java/com/example/booking/presentation/map/MapFragment.java
@@ -1,11 +1,15 @@
 package com.example.booking.presentation.map;
 
+import static androidx.databinding.DataBindingUtil.setContentView;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +17,15 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.booking.R;
+import com.kakao.vectormap.KakaoMap;
+import com.kakao.vectormap.KakaoMapReadyCallback;
+import com.kakao.vectormap.MapLifeCycleCallback;
+import com.kakao.vectormap.MapView;
 
 public class MapFragment extends Fragment {
+
+    MapView mapView;
+    KakaoMap kakaoMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +41,29 @@ public class MapFragment extends Fragment {
         editText.setOnClickListener(v -> {
             navController.navigate(R.id.action_mapFragment_to_mapSearchFragment);
         });
+
+        mapView = view.findViewById(R.id.map_view);
+        mapView.start(new MapLifeCycleCallback() {
+            @Override
+            public void onMapDestroy() {
+                // 지도 API가 정상적으로 종료될 때 호출
+                Log.d("KakaoMap", "onMapDestroy: ");
+            }
+
+            @Override
+            public void onMapError(Exception e) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출
+                Log.e("KakaoMap", "onMapError: ");
+            }
+        }, new KakaoMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull KakaoMap map) {
+                // 정상적으로 인증이 완료되었을 때 호출
+                // KakaoMap 객체를 얻어 옵니다.
+                kakaoMap = map;
+            }
+        });
+
         return view;
     }
 }
