e5924df766dcf28d2664b5ff0cde90480043b45c
hyowon <ally0204@naver.com>
2024-11-20 02:56:36 +0900

    [FEAT/#64] MVVM 구조로 API 연결 준비 - Hilt 의존성 주입

---
+++ app/src/main/java/com/example/booking/BookingApplication.java
@@ -0,0 +1,10 @@
+package com.example.booking;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class BookingApplication extends Application {
+    // Hilt 초기화 클래스
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
 
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
