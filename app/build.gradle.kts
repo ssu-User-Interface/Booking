87bc5c4a96daaffb1d9ab1d6b1a97083e2b560cb
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-11-27 23:20:38 +0900

    [FEAT/#74] RecordId 전달 구현 (#77)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     // Firebase
+    //
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation ("com.google.firebase:firebase-firestore")
 
